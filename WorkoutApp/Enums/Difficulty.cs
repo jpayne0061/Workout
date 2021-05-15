61169c4b9db3bf21da88a7525c18fcf1bce3837c
Evan Payne <jpayne0061@gmail.com>
2021-05-15 11:56:19 -0400

    more UI tweaks

--- WorkoutApp/Enums/Difficulty.cs
+++ WorkoutApp/Enums/Difficulty.cs
@@ -1,12 +1,18 @@
-﻿
+﻿using System.ComponentModel;
+
 namespace WorkoutApp.Enums
 {
     public enum Difficulty
     {
+        [Description("Easy")]
         Easy,
+        [Description("EasyMedium")]
         EasyMedium,
+        [Description("Medium")]
         Medium,
+        [Description("MediumDifficult")]
         MediumDifficult,
+        [Description("Difficult")]
         Difficult
     }
 }
